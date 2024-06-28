4e4f69d8fdf0bd59e1d8ce372833ff93c3ae3403
veis <jkvishwajeeth@gmail.com>
2024-06-28 08:41:37 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = <h1 className='title'>Getting Started React</h1>
+const inputField = 
+  <div>
+    <label htmlFor='firstname'>First Name</label>
+    <input type='text' id='firstname' placeholder='First Name' />
+  </div>
+
+const techs = ['HTML', 'CSS', 'JavaScript'] //gets appended into a string like .join(), can be used directly like ngFor
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {title}
+      {inputField}
+      {techs}
+      {techs.map((item) => (
+        <li>{item}</li>
+      ))}
+{/*       
+Map returns a new Array, while ForEach returns undefined.
+
+Here are some differences between the two methods.
+
+Map allocates memory and stores return values, while forEach throws away return values and always returns undefined.
+
+Map is chainable, but forEach isn't. This means that you can use other methods after map, but not after forEach.
+
+Map is used to transform the elements of an array, while forEach is used to loop through the elements of an array. */}
     </div>
   );
 }
